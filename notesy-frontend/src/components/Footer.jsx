67b2cf3f8a0f755c5fb7b261a460deeeb8b4e013
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-06-27 23:42:00 +0530

    landing and header added

---
+++ notesy-frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='flex h-[10vh]'>
+      <div className='w-1/5 border border-neutral-400/10 border-r-0 border-l-0'></div>
+      <div className='w-3/5 border border-neutral-400/10'></div>
+      <div className='w-1/5 border border-neutral-400/10 border-r-0 border-l-0'></div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ notesy-frontend/src/components/Landing.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './Header'
+import TextArea from './TextArea'
+import Footer from './Footer'
+
+function Landing() {
+  return (
+    <div className='h-screen w-screen bg-neutral-950 text-neutral-200 font-serif'>
+      <Header />
+      <TextArea />
+      <Footer />
+    </div>
+    
+    
+    
+  )
+}
+
+export default Landing
