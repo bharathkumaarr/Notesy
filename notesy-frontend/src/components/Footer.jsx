fd96b3938c3881095a68fcf0d145d916fe01b309
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-06-28 00:00:07 +0530

    footer update

--- notesy-frontend/src/components/Footer.jsx
+++ notesy-frontend/src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Footer() {
   return (
     <div className='flex h-[10vh]'>
-      <div className='w-1/5 border border-neutral-400/10 border-r-0 border-l-0'></div>
-      <div className='w-3/5 border border-neutral-400/10'></div>
-      <div className='w-1/5 border border-neutral-400/10 border-r-0 border-l-0'></div>
+      <div className='w-1/4 border border-neutral-400/10 border-r-0 border-l-0'></div>
+      <div className='flex w-2/4 border border-neutral-400/10 items-center justify-center'><a href="https://x.com/bharathkumxr" target='_blank'>created by @<span className='hover:underline'>bharathkumxr</span> </a></div>
+      <div className='w-1/4 border border-neutral-400/10 border-r-0 border-l-0'></div>
     </div>
   )
 }
